09f97a72d9c6af6987c3ec87f70501aa5497416a
Keith Chambers <chambers_keith@yahoo.com>
2014-12-09 06:50:27 -0800

    HTLM cleanup.

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Hello, world!</title>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
-	<style>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+	<style type="text/css">
 	body {
 		background-color: white;
 		text-align: center;
@@ -12,8 +13,8 @@
 	</style>
 </head>
 <body>
-	<img id="logo" src="logo.png" />
+	<img id="logo" src="logo.png" alt="logo.png"/>
 	<h1><?php echo "Hello, world!"; ?></h1>
-	<?php if($_ENV["HOSTNAME"]) {?><h3>Container <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
+	<?php if($_ENV["HOSTNAME"]) {?>Container <?php echo $_ENV["HOSTNAME"]; ?><?php } ?>
 </body>
 </html>
